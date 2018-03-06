c13314a3d6baa30fe054a722ab07f24b45eff205
辰木 <feibo.cfb@alibaba-inc.com>
2018-03-06 14:42:04 +0800

    update

---
+++ MyBlog/src/util/local.js
@@ -0,0 +1,18 @@
+let local = {
+	page: {
+		loginPanel:{
+			login: '登录',
+			search: '查找'
+		},
+		
+		footPanel:{
+			author: 'CasoMemory'
+		}
+		
+	},
+	dialog:{
+
+	}
+}
+
+export default local;
